aca4de40dd857fae0e8d4d05278d6e0590c8e233
kalin <kalindusupun@gmail.com>
2021-10-18 15:28:04 +0530

    Maps-1-TechincalSkillz

--- app/src/main/java/com/techincalskillz/MapFragmentActivity.java
+++ app/src/main/java/com/techincalskillz/MapFragmentActivity.java
@@ -2,24 +2,26 @@ package com.techincalskillz;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.GoogleApiAvailabilityLight;
-import com.google.android.gms.common.api.GoogleApiActivity;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -28,48 +30,66 @@ import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapFragmentActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    MapView mapView;
+
     GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map_fragment);
 
-        mapView = findViewById(R.id.mapView);
         checkPermissions(savedInstanceState);
 
-
     }
+
     private void showMap(Bundle savedInstanceState) {
 
-        // need to implement OnMapRedyCallBack and must create all 8 lifeCycles in MainActivity. eg:onstart,Onstop..
         // if you use mapfragment then lifecycle methods are not needed
         //otherwise map will not display
         // https://developers.google.com/maps/documentation/android-sdk/reference/com/google/android/libraries/maps/MapView?hl=en
+        //https://developers.google.com/maps/documentation/android-sdk/reference/com/google/android/libraries/maps/MapFragment?hl=en
 
-        mapView.getMapAsync(this);//onMapReady method automatically call
-        mapView.onCreate(savedInstanceState);
-
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_Fragment);
+        mapFragment.getMapAsync(this); //onMapReady method automatically call
     }
-
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        this.googleMap=googleMap;
-        LatLng latLng = new LatLng(6.8649,79.8997); // latitude and Longitude
+        this.googleMap = googleMap;
+        LatLng latLng = new LatLng(6.8649, 79.8997); // latitude and Longitude
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.title("My Position");
         markerOptions.position(latLng);
         googleMap.addMarker(markerOptions);
-        CameraUpdate cameraUpdate= CameraUpdateFactory.newLatLngZoom(latLng,9);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 9);
         //googleMap.moveCamera(cameraUpdate); //directly show
         googleMap.animateCamera(cameraUpdate); // moving to position
+
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setCompassEnabled(true); // Compass not showing until you rotate the map
+
+        // googleMap.getUiSettings().setZoomGesturesEnabled(false); // map cant zoom
+        googleMap.getUiSettings().setZoomGesturesEnabled(true);
+
+        //googleMap.getUiSettings().setScrollGesturesEnabled(false);// map cant Scroll
+        googleMap.getUiSettings().setScrollGesturesEnabled(true);// map cant Scroll
+
+        //googleMap.getUiSettings().setRotateGesturesEnabled(false);// map cant Rotate
+        googleMap.getUiSettings().setRotateGesturesEnabled(true);
+
+
+
+        //to get current location maker on the map
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+
     }
 
     public void checkPermissions(Bundle savedInstanceState) {
@@ -81,10 +101,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                 //google map required Google play services in the phone
                 if (checkGooglePlayServices()) {
-                    Toast.makeText(MainActivity.this, "Google play services Available", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MapFragmentActivity.this, "Google play services Available", Toast.LENGTH_SHORT).show();
                     showMap(savedInstanceState);
                 } else {
-                    Toast.makeText(MainActivity.this, "Google play services NOT  Available", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MapFragmentActivity.this, "Google play services NOT  Available", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -120,7 +140,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             Dialog dialog = googleApiAvailability.getErrorDialog(this, result, 201, new DialogInterface.OnCancelListener() {
                 @Override
                 public void onCancel(DialogInterface dialogInterface) {
-                    Toast.makeText(MainActivity.this, "User Cancelled Dialog", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MapFragmentActivity.this, "User Cancelled Dialog", Toast.LENGTH_SHORT).show();
 
                 }
             });
@@ -130,49 +150,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return false;
     }
 
-
     @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        mapView.onResume();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.none){
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+        }else if(item.getItemId()==R.id.normalMap){
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }else if(item.getItemId()==R.id.satelliteMap){
+            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }else if(item.getItemId()==R.id.mapHybrid){
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }else if(item.getItemId()==R.id.mapTerrain){
+            googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        }
+        return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-
-    }
-
-
 }
