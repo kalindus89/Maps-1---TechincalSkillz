8943f4f90f4e5078d3b8d3e9c32c43e4c9f15a1e
kalin <kalindusupun@gmail.com>
2021-10-25 19:48:56 +0530

    Maps-1-TechincalSkillz

--- app/src/main/java/com/techincalskillz/retrofit_singleton_pattern/ServiceClass.java
+++ app/src/main/java/com/techincalskillz/retrofit_singleton_pattern/ServiceClass.java
@@ -1,6 +1,7 @@
 package com.techincalskillz.retrofit_singleton_pattern;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 //calls to retrofit
@@ -10,7 +11,8 @@ public class ServiceClass {
     public static Retrofit.Builder retrofitBuilder =
             new Retrofit.Builder()
                     .baseUrl(Credentials.BASE_URl)
-                    .addConverterFactory(GsonConverterFactory.create());
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create());//make it fast
 
     public static Retrofit retrofit =retrofitBuilder.build();
 
